f34133309e3451478955d6d071642d0f3b804268
Brandon <brandon93.w@googlemail.com>
2023-07-09 17:49:22 +0200

    RomanToInteger

--- src/main/java/org/example/TwoSums.java
+++ src/main/java/org/example/TwoSums.java
@@ -1,2 +1,32 @@
-package org.example;public class TwoSums {
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSums {
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+
+    public int[] twoSumHash(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
 }
