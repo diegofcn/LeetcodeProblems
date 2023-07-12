d01f1acd8ae7fa5335652b571379f5f0e0ffe3e7
Brandon <brandon93.w@googlemail.com>
2023-07-12 15:03:32 +0200

    added

--- src/main/java/org/example/codility/FrogJump.java
+++ src/main/java/org/example/codility/FrogJump.java
@@ -12,6 +12,6 @@ public class FrogJump {
     }
 
     public static void main(String[] args) {
-        System.out.println(new FrogJump().solution(0));
+        System.out.println(new FrogJump().solution(10, 85, 30));
     }
 }
